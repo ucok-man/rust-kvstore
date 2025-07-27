e51c6e78a0e5dbf4c648e7f3223723c1f16e6f3f
Alvin Odhi N <ucokkocu411@gmail.com>
2025-07-27 15:31:31 +0700

    get user arg + structure db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::collections::HashMap;
+
+const DB_FILENAME: &str = "kv.db";
+
 fn main() {
-    println!("Hello, world!");
+    let database = Database::new().expect("Failed to create database!");
+
+    let mut args = std::env::args().skip(1);
+    let key = args.next().expect("TODO: SHOW USAGE");
+    let value = args.collect::<Vec<String>>().join(" ");
+}
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database, std::io::Error> {
+        let mut map = HashMap::new();
+        let content = std::fs::read_to_string(DB_FILENAME)?;
+        for line in content.lines() {
+            let (key, value) = line.split_once('\t').expect("DATABASE CORRUPT");
+            map.insert(key.to_owned(), value.to_owned());
+        }
+
+        Ok(Database { map: map })
+    }
 }
